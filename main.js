4c80778797c79a7b76270175978a469c607ccb53
KellerMarika <marikakeller92@gmail.com>
2022-11-02 23:10:04 +0100

    finito

--- main.js
+++ main.js
@@ -3,12 +3,12 @@
 /*** RACCOLGO GLI ELEMENTI HTML ***************************************************/
 
 /* elemento contenitore */
-const squareContaienr_El= document.getElementById("square-container");
+const squareContaienr_El = document.getElementById("square-container");
 console.log(squareContaienr_El);
 
 /* elemento square */
 //stringa contnetente elemento html costumizzato con le  variabili delle if 
-let square 
+let square
 /* color */
 let squareColor
 /* testo */
@@ -17,51 +17,35 @@ let squareText
 /**** INIZIO CILCO FOR ******************************/
 /* devo fare un contatore  che vada da 1 a 100. */
 
-for(let i=1; i<=100; i++){
-    console.log("i");
-    squareText=i;
-    squareColor= "bg-base";
+for (let i = 1; i <= 100; i++) {
 
-    if (i % 3 === 0){    
+    //console.log("i");
+    /* valori di default assegnati alle variabili globali precndeti vengono riassegnate ad ogni if*/
+    squareText = i;
+    squareColor = "bg-base";
 
-        console.log(`${i % 3 === 0}`);
-        squareText="fizz";
-        squareColor= "bg-fizz";
+    /* se è sia multiplo di 3 che di 5  */
+    //è prioritario metterlo in cima essendo le voci successive suoi sottoinsiemi (se posti in cima sottraggono alla if gli elementi, che quindi non vi entrano mai perchè non soddisfano entrambe le condizioni)
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        //console.log(`${i % 3 === 0 && i % 5 === 0}`);
+        squareText = "fizzbuzz";
+        squareColor = "bg-fizzbuzz";
+
+    } else if (i % 3 === 0) {
+
+        //console.log(`${i % 3 === 0}`);
+        squareText = "fizz";
+        squareColor = "bg-fizz";
+
+    } else if (i % 5 === 0) {
+
+        //console.log(`${i % 5 === 0}`);
+        squareText = "buzz";
+        squareColor = "bg-buzz";
     }
-
-    
-square = `<div class="square ${squareColor}">${squareText}</div>`;
-/* stampo solo alla fine */
-squareContaienr_El.innerHTML +=square;
+    /* l'elemento che verrà aggiunto avrà le caratteristiche definite in base alle if del ciclo */
+    square = `<div class="square ${squareColor}">${squareText}</div>`;
+    /* stampo solo alla fine */
+    squareContaienr_El.innerHTML += square;
 }
-
-
-
-/* if multiplo di 3 */
-//if i%3===0 color
-//  fizz
-
-
-
-
-/* if multiplo di 5 */
-//if i%5===0 color
-// buzz
-
-/* if multipli di 3 e 5 */
-//if i%3===0 && //if i%5===0  
-// fizzbuzz color
-
-/* altrimenti numero */ 
-//color 
-//num
-
-
-/* square= `<div class="square ${squareColor}"> ${i +1 }</div>` */
-
-
-//}
-
-
-
-//stampo una volta sola  ec.innerhtml= stringa + scritta + colore
