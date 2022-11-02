5d32102824f38862e2614b9d8c6c4806b1c937ac
KellerMarika <marikakeller92@gmail.com>
2022-11-02 15:29:00 +0100

    start

---
+++ main.js
@@ -0,0 +1,36 @@
+/* elemento contenitore */
+
+/* elemento square *///da inserire
+//stringa contnetente elemento html costumizzato con le  variabili delle if 
+
+/* color */
+
+
+
+/* devo fare un contatore  che vada da 1 a 100. */
+
+//for(let i=0; i>100; i++){
+
+
+/* if multiplo di 3 */
+//if i%3===0 color
+//  fizz
+
+
+
+
+/* if multiplo di 5 */
+//if i%5===0 color
+// buzz
+
+/* if multipli di 3 e 5 */
+//if i%3===0 && //if i%5===0  
+// fizzbuzz color
+
+/* altrimenti numero */ 
+//color 
+//num
+
+//}
+
+//stampo una volta sola  ec.innerhtml= stringa + scritta + colore
