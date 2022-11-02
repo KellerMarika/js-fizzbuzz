71ae27fc86e1cb2ec7871fad737682205d3cd742
KellerMarika <marikakeller92@gmail.com>
2022-11-02 22:38:46 +0100

    fizz

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
+
+
+/*** RACCOLGO GLI ELEMENTI HTML ***************************************************/
+
 /* elemento contenitore */
+const squareContaienr_El= document.getElementById("square-container");
+console.log(squareContaienr_El);
 
-/* elemento square *///da inserire
+/* elemento square */
 //stringa contnetente elemento html costumizzato con le  variabili delle if 
-
+let square 
 /* color */
+let squareColor
+/* testo */
+let squareText
 
-
-
+/**** INIZIO CILCO FOR ******************************/
 /* devo fare un contatore  che vada da 1 a 100. */
 
-//for(let i=0; i>100; i++){
+for(let i=1; i<=100; i++){
+    console.log("i");
+    squareText=i;
+    squareColor= "bg-base";
+
+    if (i % 3 === 0){    
+
+        console.log(`${i % 3 === 0}`);
+        squareText="fizz";
+        squareColor= "bg-fizz";
+    }
+
+    
+square = `<div class="square ${squareColor}">${squareText}</div>`;
+/* stampo solo alla fine */
+squareContaienr_El.innerHTML +=square;
+}
+
 
 
 /* if multiplo di 3 */
@@ -31,6 +56,12 @@
 //color 
 //num
 
+
+/* square= `<div class="square ${squareColor}"> ${i +1 }</div>` */
+
+
 //}
+
+
 
 //stampo una volta sola  ec.innerhtml= stringa + scritta + colore
